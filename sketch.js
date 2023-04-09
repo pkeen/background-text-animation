6c019b70c9153437a28fc61fed0ee84da34df6c3
pete <pkeen7@gmail.com>
2023-04-09 17:15:45 -0600

    cleanup commented out code

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-// import { Background, GlowCircle } from "./glowing_circles.js";
+
 
 import { TextBackground } from "./text-animation.js";
 
@@ -12,47 +12,15 @@ function setup() {
 
   frameRate(fr);
 
-  //bg.drawBackground();
-
-  //bg.createCircles();
-
-  
-
   bg.createTextInstances(window.innerWidth, window.innerHeight, 50);
   
 }
 
 function draw() {
 
-
-
- // ellipse(200, 200, 200, 200);
-
   bg.drawBackground();
   
   bg.drawTextInstances();
-
-  /*
-  fill(255);
-
-  for (let x = 0; x < bg._textInstances.length; x++) {
-    for (let y = 0; y < bg._textInstances[x].length; y++) {
-        bg._textInstances[x][y].renderText();
-        //fill(255);
-        //text('PK', 100, 100);
-    }
-  }
-  */
-  
-
-
-  //text(bg._text, 100, 100);
-
-  /*
-  fill(255);
-  text('pk', 100, 100);
-  */
-
 }
 
 // For some reason I had to add this to make it work
@@ -60,23 +28,3 @@ function draw() {
 window.setup = setup;
 window.draw = draw;
 
-
-
-/*
-
-import { Background } from "./glowing_circles.js";
-
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-}
-
-function draw() {
-  background(10);
-}
-
-// For some reason I had to add this to make it work
-// see: https://forum.processing.org/two/discussion/24662/script-type-module-draw-and-setup-not-working.html
-window.setup = setup;
-window.draw = draw;
-
-*/
